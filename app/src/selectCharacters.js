d1b363b3b81f68f7e477e82e3b13e87eaee50cee
hiroteru <78360123+HiraiDaiki@users.noreply.github.com>
2021-06-14 21:15:46 +0900

    selectCharacters.jsファイルを作成しました。

---
+++ app/src/selectCharacters.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+
+class selectCharacter extends React.Component {
+    render() {
+        return (
+            <div>
+                <Header />
+                <Footer />
+            </div>
+        );
+    }
+}
