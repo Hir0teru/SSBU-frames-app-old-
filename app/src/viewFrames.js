83c7f97d0695730d7da82ee0460bdafe8d4bfd7c
hiroteru <78360123+HiraiDaiki@users.noreply.github.com>
2021-06-14 21:16:04 +0900

    viewFrames.jsファイルを作成しました。

---
+++ app/src/viewFrames.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './components/Header'
+import Footer from '../src/components/Footer'
+
+class viewFrames extends React.Component {
+    render() {
+        return (
+            <div>
+                <Header />
+                <Footer />
+            </div>
+        );
+    }
+}
